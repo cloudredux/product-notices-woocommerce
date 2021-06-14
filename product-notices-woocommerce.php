37120c7afd114a8ad20480cbaa0f78aaedb94f3e
aniketdesale8 <aniketdesale8@gmail.com>
2021-06-14 16:03:47 +0530

    Translation check

--- product-notices-woocommerce.php
+++ product-notices-woocommerce.php
@@ -160,6 +160,16 @@ class CRWCPN_Main {
 
 endif;
 
+add_filter( 'load_textdomain_mofile', 'my_plugin_load_my_own_textdomain', 10, 2 );
+
+function my_plugin_load_my_own_textdomain( $mofile, $domain ) {
+    if ( 'my-domain' === $domain && false !== strpos( $mofile, WP_LANG_DIR . '/plugins/' ) ) {
+        $locale = apply_filters( 'plugin_locale', determine_locale(), $domain );
+        $mofile = WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __FILE__ ) ) . '/languages/' . $domain . '-' . $locale . '.mo';
+    }
+    return $mofile;
+}
+
 function crwcpn() {
 	return CRWCPN_Main::instance();
 }
